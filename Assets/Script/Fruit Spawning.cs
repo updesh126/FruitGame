dcd271798c19c399f01b425e40daf2d887527c84
Updesh Pr. Singh <updesh071@gmail.com>
2023-08-18 19:26:38 +0530

    Update Fruit Spawning.cs

--- Assets/Script/Fruit Spawning.cs
+++ Assets/Script/Fruit Spawning.cs
@@ -4,40 +4,40 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class FruitSpawning : MonoBehaviour
 {
     public GameObject fruitPrefab;
-    public Transform treeTransform; // Reference to the tree's Transform
-    public float spawnInterval = 2.0f; // Adjust as needed
+    public Transform treeTransform; 
+    public float spawnInterval = 2.0f; 
     private float timeSinceLastSpawn = 0.0f;
     
     private void Update()
     {
-        // Check if it's time to spawn a new fruit
+        
         timeSinceLastSpawn += Time.deltaTime;
         if (timeSinceLastSpawn >= spawnInterval)
         {
             SpawnFruit();
-            timeSinceLastSpawn = 0.0f; // Reset timer
+            timeSinceLastSpawn = 0.0f; 
         }
     }
 
     private void SpawnFruit()
     {
-        // Calculate a random position within the tree's bounds
+        
         Vector3 spawnPosition = CalculateSpawnPosition();
 
-        // Instantiate the fruit prefab at the calculated position
+        
         Instantiate(fruitPrefab, spawnPosition, Quaternion.identity);
     }
 
     private Vector3 CalculateSpawnPosition()
     {
-        // Calculate a random offset within the tree's local space
+        
         Vector3 randomOffset = new Vector3(
             Random.Range(-0.5f, 0.5f),
             Random.Range(0.0f, 1.0f),
             Random.Range(-0.5f, 0.5f)
         );
 
-        // Transform the random offset into world space using the tree's Transform
+        
         Vector3 spawnPosition = treeTransform.TransformPoint(randomOffset);
 
         return spawnPosition;
